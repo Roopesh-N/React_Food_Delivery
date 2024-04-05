0e9f1e768aa07189667770e8000d567214c4a8ff
Roopesh-N <roopeshnagineni123@outlook.com>
2024-04-05 00:31:49 -0400

    first react task

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading= React.createElement("div",{id:"child"},[React.createElement("h1",{id:"head1"},"h1 msg"),React.createElement("h2",{id:"head2"},"h2 msg")])
+const root =ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
