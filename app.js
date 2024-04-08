42994ed00e78c3d3f7d16e8e4e97490436fa482a
Roopesh-N <roopeshnagineni123@outlook.com>
2024-04-07 20:12:06 -0400

    ntg imp

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const heading= React.createElement("div",{id:"child"},[React.createElement("h1",{id:"head1"},"h1 msg"),React.createElement("h2",{id:"head2"},"h2 msg")])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const root =ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+// const nestedElement=React.createElement("div",{},[React.createElement("h1",{},"H1 message"),React.createElement("h2",{},"H2 message"),
+// React.createElement("h3",{},"H3 message")])
+// root.render(nestedElement)
+
+// const jsxNestedElement=(
+//     <div>
+//         <h1>H1 message</h1>
+//         <h2>H2 message</h2>
+//         <h3>H3 message</h3>
+//     </div>
+// )
+// const C2=()=><h1 className="superhead">H0 Message</h1>;
+
+const JsxNest=()=>(
+<header>
+  <h1>Main Page Title</h1>
+  <img src="mdn-logo-sm.png" alt="MDN logo" />
+</header>
+)
+root.render(<JsxNest/>)
+
